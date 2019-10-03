fb8466876508def6677929ccc82a8b129796b68a
hykim <hong3872@gmail.com>
2019-10-03 14:35:12 +0900

    add react handling techniques news viewer

---
+++ news-viewer/src/components/Categories/CategoriesBlock.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CategoriesBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
+`;
+
+export default CategoriesBlock;
